310974a962f8e3c38450bb31e14b8ba84c604a62
phangji <chuckychoki@gmail.com>
2014-10-03 07:53:51 +0900

    홈쇼핑 5사 테스트 - 모두 잘 동작, 이미지 퀄리티 업데이트

--- pageshotBot.js
+++ pageshotBot.js
@@ -3,7 +3,7 @@
  */
 
 var phantom = require('phantom');
-//var model = require('./modules/model');
+var model = require('./modules/model');
 var $ = require('jquery');
 var util = require('./modules/util');
 
@@ -13,11 +13,11 @@ var util = require('./modules/util');
 var createPhantom = function (pd){
     phantom.create(function (ph) {
         ph.createPage(function (page) {
-            page.set('viewportSize', {width:720,height:1080});
+            page.set('viewportSize', {width:720,height:1080});  //브라우저가 보는 크기. 창.
             console.log('start opeining page');
 
             //전처리
-            if(pd.providerId == 'HM')
+            if(pd.providerId == 'HM' || pd.providerId == 'LH')
                 util.setUserAgent(page);
             else if(pd.providerId == 'HS')
                 pd.productPgURL += '/detail';
@@ -33,14 +33,14 @@ var createPhantom = function (pd){
                     evaluateGSProductPg(page, ph, pd.id);
                 }
                 else if(pd.providerId == 'HM'){
-                    evaluateProductPg(page, ph, pd.id);
+                    evaluateHMProductPg(page, ph, pd.id);
                 }
                 else if(pd.providerId == 'HS'){
                     evaluateProductPg(page, ph, pd.id);
                 }
-// else if(pd.providerId == 'LH'){
-//                    evaluater.evaluateLHPage(page, ph);
-//                }
+                else if(pd.providerId == 'LH'){
+                    evaluateLHProductPg(page, ph, pd.id);
+                }
                 else if(pd.providerId == 'NS'){
                     evaluateProductPg(page, ph, pd.id);
                 }else{
@@ -54,30 +54,30 @@ var createPhantom = function (pd){
     });
 };
 
-createPhantom({ id : 'CJ_TEST2', productPgURL: 'http://mw.cjmall.com/m/prd/detail_cate.jsp?app_cd=PDA&item_cd=30185482&pic=TALI02',
-    providerId: 'CJ' });
+//createPhantom({ id : 'LH_TEST4', productPgURL: 'http://m.lotteimall.com/goods/viewGoodsDetail.lotte?goods_no=12458821',
+//    providerId: 'LH' });
 
 // http://www.nsmall.com/jsp/item/itemview.jsp?good_id=14431720
 // http://www.nsmall.com/jsp/item/itemview_desc.jsp?popup=y&good_id=14431720
 
 
 
-//현재 실행하는 곳
+//실행하는 곳
 
-//model.selectProductURL_Id(function (err, result) {
-//    if (err) console.error('err', err);
-//    console.log(result);
-//    var pdArr = result;
-//
-//    for(var idx in pdArr){
-//        var pdEle = pdArr[idx];
-//        setTimeout((function(pdEle){
-//            return function(){
-//                createPhantom(pdEle);
-//            }
-//        })(pdEle), 2000);
-//    }
-//});
+model.selectProductURL_Id(function (err, result) {
+    if (err) console.error('err', err);
+    console.log(result);
+    var pdArr = result;
+
+    for(var idx in pdArr){
+        var pdEle = pdArr[idx];
+        setTimeout((function(pdEle){
+            return function(){
+                createPhantom(pdEle);
+            }
+        })(pdEle), 2000);
+    }
+});
 
 // TODO : NS홈쇼핑의 경우에는, 현재 방송 중인 상품의 url로만 접근 가능.
 //createPhantom(); ==> 스케줄링 하는 곳에서 호출
@@ -93,15 +93,16 @@ function evaluateCjProductPg(page, ph, id) {
         function (result) {
             console.log(result);
 
-            setTimeout((function () {
-                return function () {
-//                    var dateStr = new Date().toString();
-
-                    page.render('../static/pageShots/' + id + '.jpeg');
-                    console.log(id + '.jpeg', 'page saved');
-                    ph.exit();
-                };
-            })(), 1000);
+//            setTimeout((function () {
+//                return function () {
+////                    var dateStr = new Date().toString();
+//
+//                    page.render('../static/pageShots/' + id + '.jpeg');
+//                    console.log('^_____^', id + '.jpeg', 'page saved');
+//                    ph.exit();
+//                };
+//            })(), 1000);
+            setTimeout(savePageShot(page, ph, id), 2000);
 
         });
 }
@@ -118,51 +119,55 @@ function evaluateGSProductPg(page, ph, id){
         function (result) {
             console.log(result);
 
-            setTimeout((function () {
-                return function () {
-                    page.render('../static/pageShots/' + id + '.jpeg');
-                    console.log(id + '.jpeg', 'page saved');
-                    ph.exit();
-
-                };
-            })(), 2000);
-
-        });
-}
-
-/*
-function evaluateHMProductPg(page, ph, id){
-    page.evaluate(function () {
-            document.body.bgColor = '#fff';
+//            setTimeout((function () {
+//                return function () {
+//                    page.render('../static/pageShots/' + id + '.jpeg');
+//                    console.log('^_____^', id + '.jpeg', 'page saved');
+//                    ph.exit();
 //
-        },
-        function () {
-//            console.log('result', result);
-
-            setTimeout((function () {
-                return function () {
-                    page.render('../static/pageShots/' + id + '.jpeg');
-                    console.log('^_____^', id + '.jpeg', 'page saved');
-                    ph.exit();
-                };
-            })(), 2000);
+//                };
+//            })(), 2000);
+            setTimeout(savePageShot(page, ph, id), 2000);
 
         });
 }
-*/
+
+
+function evaluateHMProductPg(page, ph, id){
+    setTimeout(savePageShot(page, ph, id), 2000);
+}
+
 
 function evaluateProductPg(page, ph, id){
     page.evaluate(function () {
             document.body.bgColor = '#fff';
         },
         function () {
-            setTimeout((function () {
-                return function () {
-                    page.render('../static/pageShots/' + id + '.jpeg');
-                    console.log('^_____^', id + '.jpeg', 'page saved');
-                    ph.exit();
-                };
-            })(), 2000);
+//            setTimeout((function () {
+//                return function () {
+//                    page.render('../static/pageShots/' + id + '.jpeg');
+//                    console.log('^_____^', id + '.jpeg', 'page saved');
+//                    ph.exit();
+//                };
+//            })(), 2000);
+            setTimeout(savePageShot(page, ph, id), 2000);
 
         });
+}
+
+function evaluateLHProductPg(page, ph, id) {
+    page.evaluate(function () {
+            eval('goDetail();');
+        },
+        function () {
+            setTimeout(savePageShot(page, ph, id), 2000);
+        });
+}
+
+function savePageShot(page, ph, id) {
+    return function () {
+        page.render('../static/pageShots/' + id + '.jpeg');
+        console.log('^_____^', id + '.jpeg', 'page saved');
+        ph.exit();
+    };
 }
