1b1d98cbf22927ec1eafee8b905ea98db5801c54
phangji <chuckychoki@gmail.com>
2014-11-09 01:51:57 +0900

    5초 간격으로 크롤링

--- crawlingBot.js
+++ crawlingBot.js
@@ -12,9 +12,11 @@ var chSchedule = {
     GS: "http://m.gsshop.com/tv/tvScheduleMain.gs",
     HM: "http://m.hyundaihmall.com/front/index.do",   //userAgent 필요
     HS: "http://m.hnsmall.com/display/tvtable.do",
-    LH: "http://m.lotteimall.com/main/viewMain.lotte",
+    LH: "http://m.lotteimall.com/main/viewMain.lotte#3",
     // http://m.lotteimall.com/main/viewMain.lotte?mlog=90010_5#33 -- ajax
     NS: "http://www.nsmall.com/jsp/etv/shopping_schedule.jsp"  // 모바일 페이지가 없음
+//    NS : 'http://www.nsmall.com/jsp/etv/shopping_schedule.jsp?sel_date=' + util.getDateStrAfter(1)
+
 };
 
 var FROM_NOW = {
@@ -35,13 +37,13 @@ var createPhantom = function (channel, fromNow) {
                 if (channel == 'CJ') {
                     switch (fromNow) {
                         case FROM_NOW.TODAY :
-                            evaluater.evaluateCJPage(page, ph);
+//                            evaluater.evaluateCJPage(page, ph);
                             break;
                         case FROM_NOW.TOMORROW :
 //                            evaluater.evaluateCJPageAhead(page, ph);
                             break;
                         case FROM_NOW.TDAT :
-//                            evaluater.evaluateCJPageAhead2(page, ph);
+                            evaluater.evaluateCJPageAhead2(page, ph);   //CJ는 3일을 한번에
                             break;
                     }
                 } else if (channel == 'GS') {
@@ -102,10 +104,10 @@ var createPhantom = function (channel, fromNow) {
                             evaluater.evaluateNSPage(page, ph);
                             break;
                         case FROM_NOW.TOMORROW :
-//                            evaluater.evaluateNSPageAhead(page, ph);
+                            evaluater.evaluateNSPageAhead(page, ph);
                             break;
                         case FROM_NOW.TDAT :
-//                            evaluater.evaluateNSPageAhead2(page, ph);
+                            evaluater.evaluateNSPageAhead2(page, ph);
                             break;
                     }
                 } else {
@@ -118,31 +120,37 @@ var createPhantom = function (channel, fromNow) {
 };
 
 
-var args = process.argv.slice(2);
-var args2 = process.argv.slice(3);
+//var args = process.argv.slice(2);
+//var args2 = process.argv.slice(3);
 //createPhantom(args);    //node crawlingBot 'CJ'
 
 
 
 var excuteBot = function () {
 
-//    createPhantom('CJ', FROM_NOW.TOMORROW);
+//    createPhantom('CJ', FROM_NOW.TODAY);
+//    createPhantom('GS', FROM_NOW.TODAY);
 //    createPhantom('GS', FROM_NOW.TOMORROW);
+//    createPhantom('GS', FROM_NOW.TDAT);
 //    createPhantom('HM', FROM_NOW.TODAY);
-//    createPhantom('HS', FROM_NOW.TDAT);
-//    createPhantom('LH', FROM_NOW.TODAY);
-//    createPhantom('NS', FROM_NOW.TOMORROW);
+//    createPhantom('HS', FROM_NOW.TODAY);
+//    createPhantom('LH', 0);
+//    createPhantom('LH', 1);
+//    createPhantom('LH', 2);
+//    createPhantom('LH', 1);
+//    createPhantom('LH', 2);
+//    createPhantom('NS', 2);
 
-    var fromNow = FROM_NOW.TDAT;
-    for (var key in chSchedule) {
-        (function(key){
-            console.log('********** key **********', key, '**********', Object.keys(FROM_NOW)[fromNow], '**********');
-            setTimeout(function () {
-                createPhantom(key, fromNow);
-            }, 5000);
-        })(key);
-    }
-/*
+//    var fromNow = 0;
+//    for (var key in chSchedule) {
+//        (function(key){
+//            console.log('********** key **********', key, '**********', Object.keys(FROM_NOW)[fromNow], '**********');
+//            setTimeout(function () {
+//                createPhantom(key, fromNow);
+//            }, 5000);
+//        })(key);
+//    }
+
     for (var keyNow in FROM_NOW){
 
         for (var key in chSchedule) {
@@ -154,7 +162,7 @@ var excuteBot = function () {
             })(key);
         }
     }
-*/
+
 };
 
 excuteBot();
